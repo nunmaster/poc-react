30ecd964683987aa6657566d077efec6048491b4
Pham Manh Hung <hung.pham@ascendcorp.com>
2017-10-30 18:08:01 +0700

    [Image] set image at center

--- containers/Image/index.js
+++ containers/Image/index.js
@@ -6,7 +6,7 @@ import {ascendImage} from "~/assets";
 export default class extends Component {
 	render() {
 		return (
-			<View style={{alignItems: 'center'}}>
+			<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
 				<Image
 					source={ascendImage}
 					style={{ width: 225, height: 225 }}
