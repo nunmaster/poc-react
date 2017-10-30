c8f1d011ce464d6d9f2e23983adc84cd99bd8ad0
Pham Manh Hung <hung.pham@ascendcorp.com>
2017-10-30 17:52:04 +0700

    added image

---
+++ assets/index.js
@@ -0,0 +1,1 @@
+export const ascendImage = require('./ascend.png');
--- containers/Image/index.js
+++ containers/Image/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { View, Image} from "react-native";
+
+import {ascendImage} from "~/assets";
+
 export default class extends Component {
 	render() {
 		return (
 			<View style={{alignItems: 'center'}}>
 				<Image
-					source={require('./ascend.png')}
+					source={ascendImage}
 					style={{ width: 225, height: 225 }}
 				/>
 			</View>
