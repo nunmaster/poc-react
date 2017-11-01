2761147fa3e9ba111927694b9f032dc18435f470
Pham Manh Hung <hung.pham@ascendcorp.com>
2017-11-01 13:26:52 +0700

    update image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,5 @@
 export default {
   avatar: require('./avatar.png'),
   laptop_phone: require('./laptop_phone_howitworks.png'),
-  ascendImage = require('./ascend.png')
+  ascendImage: require('./ascend.png')
 };
--- src/container/Image/index.js
+++ src/container/Image/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { Text, View, Image} from "react-native";
-import ascendImage from '../../assets/images';
+import images from '../../assets/images';
 export default class extends Component {
 	render() {
 		return (
 			<View style={{alignItems: 'center'}}>
 				<Image
-					source={ascendImage}
+					source={images.ascendImage}
 					style={{ width: 225, height: 225 }}
 				/>
 			</View>
