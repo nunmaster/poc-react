4783ab6ce81510c043b96474b0b8b75c5a04b366
Nonthanun Sudlapa <nonthanun.sud@ascendcorp.com>
2017-11-02 02:08:36 +0700

    Add notification for iOS

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import Preload from './src/container/Preload';
 import { configStore } from './src/store';
 import { resetTo } from './src/store/actions/common';
 import { initRoute, authorizedRoute } from './src/constants/routes';
+import firebase from 'react-native-firebase';
 // import CodePush from "react-native-code-push";
 
 class Root extends React.Component {
@@ -14,8 +15,20 @@ class Root extends React.Component {
     isLoading: true
   };
 
-  componentDidMount() {    
+  componentDidMount() {   
+    firebase.messaging().requestPermissions() 
 
+    firebase.messaging().onMessage((message) => console.log('message', message));
+    // This gets logged only when the app is open
+    // Nothing happens when app is closed, no notification is shown either
+
+
+    firebase.messaging().getInitialNotification()
+      .then((notification) => {
+        console.log('Notification which opened the app: ', notification);
+      });
+    // This is never called
+    
     configStore(store => {
       if(!__DEV__){
         const firstRoute = store.getState().auth.loggedIn ? authorizedRoute : initRoute;
@@ -24,8 +37,6 @@ class Root extends React.Component {
       this.store = store;
       this.setState({ isLoading: false });
     });
-
-    
   }
 
   shouldComponentUpdate(nextProps, { isLoading }) {
